562037ca28a8710659bad6253079c5cb00f7d148
tliu <15118063927@163.com>
2019-03-24 17:34:12 +0800

    新增lambda表达式

---
+++ LeetCode/src/main/java/com/java8/newProperty/LambdaTest.java
@@ -0,0 +1,23 @@
+package com.java8.newProperty;
+
+public class LambdaTest {
+    final String constant = "sd";
+    public static void main(String[] args) {
+        LambdaTest test = new LambdaTest();
+        MathOperation addOperaton = (int a,int b) -> a+b;
+        test.print(3,4, addOperaton);
+        StringLambda operation = s -> System.out.println(s);
+        operation.call(test.constant);
+    }
+
+    interface MathOperation {
+        int operation(int a,int b);
+    }
+
+    interface StringLambda {
+        void call(String message);
+    }
+    private void print(int a, int b, MathOperation operation) {
+        System.out.println(operation.operation(a,b));
+    }
+}
